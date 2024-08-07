0198534fce847f43285dba2f4b93a86f63a62d66
Morenayd <tofdebby@gmail.com>
2024-08-07 05:26:46 +0100

    Initial Commit

---
+++ MatchFriends/MatchFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchFriendsApp.swift
+//  MatchFriends
+//
+//  Created by Jesutofunmi Adewole on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
